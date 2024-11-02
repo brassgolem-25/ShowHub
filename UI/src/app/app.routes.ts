f6f80a408a40d1d2f8131a9f4c16bc4889f87ac0
Tushar Tiwari <tushartiwari2002@gmail.com>
2024-11-03 01:49:09 +0530

    Service added

--- UI/src/app/app.routes.ts
+++ UI/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './shared/home/home.component';
+import { MovieDetailComponent } from './shared/movie-detail/movie-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'explore/home',component:HomeComponent},
+    {path:'explore/movies',component:MovieDetailComponent},
+    {path:'**',redirectTo:'explore/home',pathMatch:'full'}
+];
---
+++ UI/src/app/core/movie.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Movie } from '../shared/types/movie';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  movieObservable:any;
+  constructor(private http:HttpClient) { 
+    this.movieObservable = this.getAllMovies();
+  }
+
+
+  getRecommendedMovies():Observable<Movie[]>{
+    return this.http.get<Movie[]>('http://localhost:3000/api/movies/few');
+  }
+
+  getAllMovies():Observable<Movie[]> {
+    return this.http.get<Movie[]>('http://localhost:3000/api/movies/all');
+  }
+
+  getFilteredMovies(filterArr:string[]){
+    return this.movieObservable.pipe().subscribe
+  }
+}
