77400f93859980e87f019121bcb08db437bbca2f
Tushar Tiwari <tushartiwari2002@gmail.com>
2024-12-03 00:39:31 +0530

    Changes for fetching cities List

--- BE/controllers/cityController.js
+++ BE/controllers/cityController.js
@@ -1,17 +1,18 @@
 import { postgreClient } from '../config/postgre.js';
+import { redisClient } from "../config/redis.js";
 
 export const insertCities = async (req, res) => {
     try {
         const requestData = req.body;
         for (let obj of requestData) {
             const { name, state } = obj;
-            const isCityPresent = (await postgreClient.query(`select count(*) from indianCities where name=$1`,[name])).rows[0].count;
-            if(isCityPresent > 0){
+            const isCityPresent = (await postgreClient.query(`select count(*) from indianCities where name=$1`, [name])).rows[0].count;
+            if (isCityPresent > 0) {
                 console.log(`City "${name}" is already present. Skipping insertion.`);
                 continue;
             }
             const sqlQuery = `INSERT INTO indianCities(name,state) VALUES($1,$2)`;
-            await postgreClient.query(sqlQuery,[name,state]);
+            await postgreClient.query(sqlQuery, [name, state]);
         }
         await postgreClient.query("COMMIT;");
         res.json({ 'status': 'ok' });
@@ -22,11 +23,18 @@ export const insertCities = async (req, res) => {
     }
 }
 
-export const getCitiesList = async(req,res) =>{
-    try{
+export const getCitiesList = async (req, res) => {
+    try {
+        const redisResponse = await redisClient.sMembers('citiesList');
+        console.log(redisResponse.length)
+        if (redisResponse.length > 0) {
+            return res.json(redisResponse);
+        }
         const result = (await postgreClient.query(`select distinct name from indianCities order by name`)).rows;
+        const cityNames = result.map(city => city.name);
+        const redisRes = await redisClient.sAdd('citiesList', cityNames);
         res.json(result);
-    }catch(error){
-        return res.status(500).json({ message: error.message });    
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
     }
 }
