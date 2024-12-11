b52108cfa519e88012735073bd71dee2e37d22f3
Tushar Tiwari <tushartiwari2002@gmail.com>
2024-12-12 00:39:50 +0530

    Updated ShowTime Controller

--- BE/controllers/showTimeController.js
+++ BE/controllers/showTimeController.js
@@ -9,45 +9,47 @@ export class ShowTimeController {
   // Get showTime for a movie
   static getShowTimeDetails = async (req, res) => {
     try {
-      const { imdbID ,city} = req.query;
-      const {startDate,endDate} = this.getFormattedDate();
-      if(!imdbID  || !city ){
-        return res.status(500).json({message:'Parameters Missing'});
+      const { imdbID, city, date } = req.query;
+      const formattedDate = `${date.slice(0, 4)}-${date.slice(4, 6)}-${date.slice(6)}`;
+      if (!imdbID || !city) {
+        return res.status(500).json({ message: 'Parameters Missing' });
       }
-      const theaterDetails = (await Theater.findOne({'location.city':city},{location:1,name:1,theaterID:1,_id:0}));
-      const movieDetails = (await Movie.findOne({imdbID},{_id:0,title:1,genre:1}))
-      const showTimeSearchQuery = {theaterID:theaterDetails.theaterID, imdbID: imdbID , date :{$gte:startDate,$lte:endDate} };
-      const showTime = await Showtime.find(showTimeSearchQuery, { __v: 0, _id: 0 });
-      const showTime_Theater_Details  = showTime.map(shows => {
-        return {
-          ...shows.toObject(),
-          movie_name : movieDetails.title,
-          movie_genre:movieDetails.genre,
-          theater_name: theaterDetails.name,
-          theater_location:theaterDetails.location,
-          theater_amenities:"Free Parking,Lunch",
-          cancellationAvailable:true
+      const theaterDetails = (await Theater.find({ 'location.city': city }, { location: 1, name: 1, theaterID: 1, _id: 0 }));
+      const movieDetails = (await Movie.findOne({ imdbID }, { _id: 0, title: 1, genre: 1, language: 1 }))
+      let showTime_Theater_Details = [];
+      for (let theater of theaterDetails) {
+        const showTimeSearchQuery = { theaterID: theater.theaterID, imdbID, date: formattedDate };
+        const showTime = await Showtime.find(showTimeSearchQuery, { format:1,time:1,price:1,availableSeats:1, _id: 0});
+        const showTimeForTheater = {
+          showTime,
+          theater_name: theater.name,
+          theater_location: theater.location,
+          theater_amenities: "M-Ticket,Food & Beverages",
+          cancellationAvailable: true
         }
-      })
 
-      console.log(showTime_Theater_Details.slice(0,1))
-      return res.status(200).json(showTime_Theater_Details);
+        showTime_Theater_Details.push(showTimeForTheater)
+      }
+
+
+      return res.status(200).json({ status: "success", result: { movieDetails: movieDetails, theater_showtime: showTime_Theater_Details } });
       // return res.status(200).json({status:"ok"})
     } catch (error) {
-      res.status(500).json({ error: error.message });
+      console.log(error);
+      res.status(500).json({ status: "false", error: error.message });
     }
   }
 
-  static getFormattedDate(){
+  static getFormattedDate() {
     const currentDate = new Date();
 
-    const startOfWeekDate = startOfWeek(currentDate, { weekStartsOn: 1 }); 
-    const endOfWeekDate = endOfWeek(currentDate, { weekStartsOn: 1 });     
-    
+    const startOfWeekDate = startOfWeek(currentDate, { weekStartsOn: 1 });
+    const endOfWeekDate = endOfWeek(currentDate, { weekStartsOn: 1 });
+
     // Format dates as YYYYMMDD
     const startDate = format(startOfWeekDate, 'yyyy-MM-dd');
     const endDate = format(endOfWeekDate, 'yyyy-MM-dd');
 
-    return {startDate,endDate}
+    return { startDate, endDate }
   }
 }
